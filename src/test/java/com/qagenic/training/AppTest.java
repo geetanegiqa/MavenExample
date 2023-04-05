ccc924b4faefc39702862fc9ade10c0d6a88d39d
Geeta Negi <geeetanegiqa@gmail.com>
2023-04-05 21:42:50 +0530

    added print text to AppTest.java

---
+++ src/test/java/com/qagenic/training/AppTest.java
@@ -0,0 +1,21 @@
+package com.qagenic.training;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("AppTest is successful");
+        assertTrue( true );
+    }
+}
